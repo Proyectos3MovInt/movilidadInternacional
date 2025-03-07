37b44426aecd720dbb5dfc5d0b75075ba213cf57
carmonalanzasalvaro <119354379+carmonalanzasalvaro@users.noreply.github.com>
2025-03-07 05:01:45 +0100

    Añadido método put a form.js y formulario actualizado para la actualización dinámica de campos

--- src/components/formulario/InputField.jsx
+++ src/components/formulario/InputField.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { updateForm } from "@/lib/form.js";
 
 export function InputField({
   label,
@@ -7,10 +8,23 @@ export function InputField({
   type = 'text',
   required = false,
 }) {
+  
+  const handleBlur = async (event) => {
+    const value = event.target.value;
+    if (value.trim() !== "") {
+      await updateForm(name, value);
+    }
+  };
+
   return (
     <div>
       <label className="block text-gray-700">{label}</label>
-      <input {...register(name, { required })} type={type} className="w-full p-2 border rounded" />
+      <input 
+        {...register(name, { required })} 
+        type={type} 
+        className="w-full p-2 border rounded" 
+        onBlur={handleBlur}
+      />
     </div>
   );
 }
--- src/lib/form.js
+++ src/lib/form.js
@@ -1,17 +1,16 @@
 'use server';
 
-import { cookies } from "next/headers"
+import { cookies } from "next/headers";
 
 export async function getForm() {
   try {
     const cookieStore = await cookies();
     const jwt_token = cookieStore.get("jwt-token").value;
 
-
     const response = await fetch("https://amused-danya-hugobarea-b3e72b1a.koyeb.app/form", {
       method: "GET",
       headers: {
-        "Authorization": `Bearer ${token}`,
+        "Authorization": `Bearer ${jwt_token}`,
         "Content-Type": "application/json"
       }
     });
@@ -23,6 +22,31 @@ export async function getForm() {
     return await response.json();
   } catch (error) {
     console.error("Error en getFormData:", error);
+    return null;
+  }
+}
+
+export async function updateForm(fieldName, value) {
+  try {
+    const cookieStore = await cookies();
+    const jwt_token = cookieStore.get("jwt-token").value;
+
+    const response = await fetch("https://amused-danya-hugobarea-b3e72b1a.koyeb.app/form", {
+      method: "PUT",
+      headers: {
+        "Authorization": `Bearer ${jwt_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ [fieldName]: value }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al actualizar el campo del formulario");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error en updateForm:", error);
     return null;
   }
 }
