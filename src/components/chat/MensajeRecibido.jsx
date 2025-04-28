3c07f1d725d8e417518e9f10ddc5133f0a4f81d5
Hugo Barea <bareamorenohugo@gmail.com>
2025-04-28 10:30:17 +0200

    Completar implementación del chat

--- src/components/chat/MensajeRecibido.jsx
+++ src/components/chat/MensajeRecibido.jsx
@@ -5,7 +5,7 @@ export default function MensajeRecibido( { msg }) {
         <div className="inline-flex justify-start items-start gap-2">
             <div className="p-2 bg-blue-300 rounded-3xl flex justify-start items-center gap-2">
                 <div className="w-6 h-6 relative overflow-hidden">
-                    <Icons.PersonWhite />
+                    <Icons.PersonWhiteChat />
                 </div>
             </div>
             <div className="px-4 py-2 bg-white rounded-lg outline outline-1 outline-offset-[-1px] outline-blue-300 flex justify-center items-center gap-2">
